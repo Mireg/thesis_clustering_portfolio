3bb84c918eb453d68ece105287d689a87c964894
mireg <k.miranowski@gmail.com>
2025-02-19 15:29:00 +0100

    data

--- Scripts/tickers_scrapper.py
+++ Scripts/tickers_scrapper.py
@@ -5,11 +5,10 @@ import pandas as pd
 #change the url if you want to get historical list
 #Current list: https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
 #2010:  http://web.archive.org/web/20100210015201/https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
-#2015: 
-#2017:
 #2019: http://web.archive.org/web/20190107004442/https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
+#2025: http://web.archive.org/web/20250105183541/https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
 
-url = "http://web.archive.org/web/20190107004442/https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+url = "http://web.archive.org/web/20250105183541/https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
 def get_sp500_tickers(url):
     response = requests.get(url)
@@ -29,4 +28,4 @@ def get_sp500_tickers(url):
 tickers = get_sp500_tickers(url)
 
 df = pd.DataFrame(tickers, columns=["Ticker"])
-df.to_csv("Data/sp500_tickers_2019.csv", index=False)
+df.to_csv("Data/sp500_tickers_2025.csv", index=False)
